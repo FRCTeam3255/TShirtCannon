9afcf4da45d188fcf74b2e4ea533ae4bd42c29b8
Tyler <Tyler@192.168.1.122>
2014-09-05 17:52:27 -0700

    Added Drivetrain.java to subsystems and began setting up its constructor.

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -52,6 +52,7 @@ public class RobotMap {
     public static final int CONTROLLER_DRIVER = 1;
     public static final int JOYSTICK_SHOOTER = 2;
     
+    
     // Variables
     // The wheel is a 4 in diameter
     // Therefore, distance travelled/wheel rot:
